0ab52b4edbf38a6ce4ad2de77c3181edf4ff8ebb
Ylane <ylanebaha@protonmail.com>
2021-12-11 01:47:18 +0100

    Je crois que le script est bon

--- prozac.sh
+++ prozac.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 LINE=`wc -l links`
-LINE=${LINE:0:1}
-LINE=`expr $RANDOM % $LINE`
-echo $LINE
-
+LINE=${LINE:0:1} #Combien de lignes dans le fichier
+LINE=`expr $RANDOM % $LINE` #Aleatoire et modoluisé
 i=0
-
 while read -r line;
 do
 	if [ $i -eq $LINE ]
 	then
-		nohup firefox "$line" > /dev/null
+		nohup firefox "$line" > /dev/null &
 		echo Enjoy
 		exit
 	fi
 	i=`expr $i + 1`
 done < links
-
+#La depression est à present guérie
